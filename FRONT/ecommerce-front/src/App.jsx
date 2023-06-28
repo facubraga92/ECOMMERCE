1128cceffa9980a5bd382aa84859a6d344429e2b
Brandon Miguel Castillo Llajamango <pibelanzallamas@MacBook-Pro-de-Brandon.local>
2023-06-28 00:30:42 -0300

    Rutas Front

--- FRONT/ecommerce-front/src/App.jsx
+++ FRONT/ecommerce-front/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Route, Routes } from "react-router";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Navbar />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,8 +30,17 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/users/:id"} element={<Profile />} />
+        <Route path={"/products/all"} element={<Products />} />
+        <Route path={"/products/:type"} element={<Product />} />
+        <Route path={"/products/search/:name"} element={<Search />} />
+        <Route path={"/faq"} element={<Faq />} />
+        <Route path={"/contact"} element={<Contact />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
