b31c7856b18ba638ccf0df68962797608499af3f
Sebastian <sebastian_buscaglia@outlook.com>
2023-07-04 18:04:44 -0300

    --

--- FRONT/ecommerce-front/src/App.jsx
+++ FRONT/ecommerce-front/src/App.jsx
@@ -7,6 +7,7 @@ import { useSelector } from "react-redux";
 import SingleProduct from "./commons/SIngleProduct";
 import Navbar from "./commons/Navbar";
 import CartHistory from "./components/CartHistory";
+import Cart from "./commons/Cart";
 
 function App() {
   const user = useSelector((state) => state.user);
@@ -19,14 +20,11 @@ function App() {
         <Route path={"/login"} element={<Login />} />
         <Route path={"/products/all"} element={<ProductsGrid />} />
         <Route path={"/products/:id"} element={<SingleProduct />} />
-<<<<<<< HEAD
         <Route path={"/cart"} element={<Cart />} />
         <Route
           path={"/cart-history"}
           element={user.email ? <CartHistory /> : <Login />}
         />
-=======
->>>>>>> 6e1a9a9159fbefff6e0b05af9bef51e1b4b450c2
       </Routes>
     </>
   );
