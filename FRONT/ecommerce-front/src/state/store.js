c31c95d07b55e5208449ea50ea693330304b5faa
justomaldonado <maldonadojusto99@gmail.com>
2023-07-07 00:35:54 -0300

    'feature/search-products'

--- FRONT/ecommerce-front/src/state/store.js
+++ FRONT/ecommerce-front/src/state/store.js
@@ -2,9 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { userReducer } from "./user";
 import { cartReducer } from "./cart";
+import { productsReducer } from "./products";
 
 const store = configureStore({
-  reducer: { user: userReducer, cart: cartReducer },
+  reducer: { user: userReducer, cart: cartReducer, products: productsReducer },
 });
 
 export default store;
